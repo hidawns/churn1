67af4eaece4d97892d18cd6a592474a267e0f0ad
hidawns <134594505+hidawns@users.noreply.github.com>
2025-07-20 10:42:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,16 +20,11 @@ st.title("Customer Churn Prediction")
 
 # === Input: Binary Features ===
 gender = st.selectbox("Gender", ['Female', 'Male'])
+senior_citizen = st.selectbox("Is Senior Citizen?", ['Yes', 'No'])
 partner = st.selectbox("Has Partner?", ['Yes', 'No'])
 dependents = st.selectbox("Has Dependents?", ['Yes', 'No'])
 phone_service = st.selectbox("Has Phone Service?", ['Yes', 'No'])
 paperless_billing = st.selectbox("Uses Paperless Billing?", ['Yes', 'No'])
-senior_citizen = st.selectbox("Is Senior Citizen?", ['Yes', 'No'])
-
-# === Input: Numeric Features ===
-tenure = st.number_input("Tenure (months)", min_value=0)
-monthly_charges = st.number_input("Monthly Charges", min_value=0.0)
-total_charges = st.number_input("Total Charges", min_value=0.0)
 
 # === Input: Multi-category ===
 multiple_lines = st.selectbox("Multiple Lines", ['No', 'Yes', 'No phone service'])
@@ -46,17 +41,20 @@ payment_method = st.selectbox("Payment Method", [
     'Bank transfer (automatic)', 'Credit card (automatic)'
 ])
 
+# === Input: Numeric Features ===
+tenure = st.number_input("Tenure (months)", min_value=0)
+monthly_charges = st.number_input("Monthly Charges", min_value=0.0)
+total_charges = st.number_input("Total Charges", min_value=0.0)
+
 # === Build input dict ===
 input_dict = {
+    #binary features
     'gender': 1 if gender == 'Male' else 0,
+    'SeniorCitizen': 1 if senior_citizen == 'Yes' else 0,
     'Partner': 1 if partner == 'Yes' else 0,
     'Dependents': 1 if dependents == 'Yes' else 0,
     'PhoneService': 1 if phone_service == 'Yes' else 0,
     'PaperlessBilling': 1 if paperless_billing == 'Yes' else 0,
-    'SeniorCitizen': 1 if senior_citizen == 'Yes' else 0,
-    'tenure': tenure,
-    'MonthlyCharges': monthly_charges,
-    'TotalCharges': total_charges,
 
     # One-hot encoded features (initialize below)
     f"MultipleLines_{multiple_lines}": 1,
@@ -69,6 +67,11 @@ input_dict = {
     f"StreamingMovies_{streaming_movies}": 1,
     f"Contract_{contract}": 1,
     f"PaymentMethod_{payment_method}": 1,
+
+    # numeric features
+    'tenure': tenure,
+    'MonthlyCharges': monthly_charges,
+    'TotalCharges': total_charges,
 }
 
 # === Build input dataframe ===
@@ -90,6 +93,6 @@ if st.button("Predict Churn"):
     probability = model.predict_proba(input_df)[0][1]
 
     if prediction == 1:
-        st.error(f"❌ Customer likely to churn. Probability: {probability:.2f}")
+        st.error(f"❌ Customer likely to churn. Probability to churn: {probability:.2f}")
     else:
-        st.success(f"✅ Customer likely to stay. Probability: {probability:.2f}")
+        st.success(f"✅ Customer likely to stay. Probability to churn: {probability:.2f}")
